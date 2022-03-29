99cfb530268d4aa0cd25359fc9f52a2b6f496dcc
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-03-29 11:51:36 +0700

    noidung

---
+++ src/Point/Main.java
@@ -0,0 +1,14 @@
+package Point;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point2D point2d = new Point2D(15, 12);
+        System.out.println(point2d);
+
+
+        Point3D point3d = new Point3D(15, 12, 98);
+        System.out.println(point3d);
+    }
+}
---
+++ src/Point/Point2D.java
@@ -0,0 +1,37 @@
+package Point;
+
+public class Point2D    {
+    private float x=0.0f;
+    private float y=0.0f;
+
+    public Point2D() {
+    }
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Point2D (" + x + ", " + y + ")";
+    }
+
+}
